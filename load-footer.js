a739f28279406dc01de1dcbbb1b4a0ffe738f8d9
Manan Malhotra <86295461+Manan-Malhotra-VFX@users.noreply.github.com>
2024-09-02 23:47:49 +0530

    Update load-footer.js

--- load-footer.js
+++ load-footer.js
@@ -1,1 +1,9 @@
-
+// load-footer.js
+document.addEventListener('DOMContentLoaded', function() {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('beforeend', data);
+        })
+        .catch(error => console.error('Error loading footer:', error));
+});
